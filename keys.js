1c68432ca4295a1cf099138a0a813f2f1631c019
Unknown <timpike22@gmail.com>
2017-09-27 11:31:05 -0700

    updated but not finished

---
+++ keys.js
@@ -0,0 +1,17 @@
+var twitterKeys = {
+  consumer_key: 'kwDTUr2avpdQhPWJbttJ5cSfg',
+  consumer_secret: 'gwc6Q9ruOzbpaKAPzAOOcFlHu0KWu7EGPmsVWkD6D7uZbdDqHh',
+  access_token_key: '910271176487792640-RarMSaB40ZWgDsW0NbLBCU2q60jPgZR',
+  access_token_secret: 'MPuSXKbX3Fxzldgma0s0L8bP4tkW29hukpmkrCLX4rLtP'
+}
+
+var spotifyKeys = {
+  id: 'f230b8352c1247b8812e41665877f600',
+  secret: 'e67066f631494178953e980c8f3b05e8'
+}
+
+module.exports = {
+
+    twitterKeys : twitterKeys,
+    spotifyKeys : spotifyKeys
+};
---
+++ liri.js
@@ -0,0 +1,108 @@
+var fs = require ("fs");
+var twitter = require ("twitter");
+var spotify = require ("spotify");
+var request = require ("request");
+var keys = require('./keys.js');
+var argVar1 = process.argv[2];
+var argVar2 = process.argv[3];
+
+function initializer(){
+    switch(argVar1){
+
+    case "my_tweets":
+    myTweets();
+    break;
+    
+    case "spotify_this_song":
+    spotify_song();
+    break;
+
+    case "check_this_movie":
+    check_movie();
+    break;
+    
+    default:
+    console.log("Please enter a command (i.e. my_tweets | spotify_this_song | check_this_movie");
+
+    };
+
+};
+
+function myTweets(){
+    var client = new twitter(keys.twitterKeys);
+    var params = { screen_name: 'dbai22', count: 20 };
+    client.get('statuses/user_timeline', params, function(error, tweets, response){
+        if(error){
+        return console.log("Error");
+
+         } else {
+            for(var i = 0; i < tweets.length; i++){
+            console.log(i+1);
+            console.log(tweets[i].text);
+            console.log(tweets[i].created_at);
+            }
+        }
+    });
+};
+
+
+function check_movie(){
+    if(argVar2 === undefined){
+       argVar2 = "Mr.Nobody";
+   }
+   queryUrl = "http://www.omdbapi.com/?t=" + argVar2 + "&y=&plot=short&apikey=40e9cece";
+   
+    
+    request(queryUrl, function(error, response, body){
+    if (!error && response.statusCode == 200) {
+        let json = JSON.parse(body);
+        console.log("Title: "+ json.Title);
+        console.log("Year: " + json.Year);
+        console.log("IMDB Rating: " + json.imdbRating);
+        console.log("Country: " + json.Country);
+        console.log("Language: " + json.Language);
+        console.log("Plot: " + json.Plot);
+        console.log("Actors: " + json.Actors);
+        console.log("Rotten Tomatoes Rating: " + json.tomatoRating);
+        console.log("Rotten Tomatoes URL: " + json.tomatoURL);
+      } else {
+          console.log("Oops! try again!");
+      }
+    });
+   };
+
+
+
+function spotify_song(){
+   // var queryTrack;
+    if(argVar2 == undefined){
+        argVar2 = "I Want It That Way";
+    }
+    
+        
+    spotify.spotifyKeys.search({ type: 'track', query: argVar2, count: 20 }, function(error, data){
+        if(error){
+           return console.log("error");
+      } else  {
+          console.log(data.tracks.items);
+      }
+    });
+          //  var trackInfo = data.tracks.items;
+           // console.log(trackInfo);
+            
+           // for (i = 0; i < trackInfo.length; i++){
+                //console.log(i + 1)
+           //     console.log("Track: " + trackInfo[i].name);
+            //    console.log("Artist: " + trackInfo[i].artists[0].name);
+           //     console.log("Album: " + trackInfo[i].album.name);
+           //     console.log("Link: " + trackInfo[i].href);
+         //       console.log("\n ------------------");       
+              //  }
+          //  }
+        
+     //   });
+ }
+
+
+initializer();
+
